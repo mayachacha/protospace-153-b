f26419436285c457e54881ce6b6bb331411d4e51
mayachacha <asakawa926@gmail.com>
2023-08-04 19:23:37 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,20 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+    @protype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  private
+  def prototype_params
+    params.require(:protype).permit(:title, :catch_copy, :concept, :user)
+  end
+
+
 end

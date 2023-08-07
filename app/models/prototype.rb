0b6e3586e8b7d86482b1878b4297a7af24e5361e
mayachacha <asakawa926@gmail.com>
2023-08-07 15:32:35 +0900

    画像投稿機能の実装、アソシエーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,8 @@
 class Prototype < ApplicationRecord
   validates :title, presence: true 
   validates :catch_copy, presence: true
-  validates :concept, presence: true  
+  validates :concept, presence: true
+  
+  has_one_attached :image
+  belongs_to :user
 end

02d1cab712894b93526e9278ea7e3f17147d1b0f
mayachacha <asakawa926@gmail.com>
2023-08-08 17:40:10 +0900

    コントローラー編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :move_to_index, except: [:index, :show, :new ,:destroy]
+  before_action :move_to_index, except: [:index, :show, :new ,:destroy, :create]
 
   
   def index
